500e17376d430b8617f12c8cc60a09f71b960da0
fennerspohr <isadorafennerspohr@gmail.com>
2024-10-20 20:09:52 -0300

    initial stack project

--- teste.hs
+++ teste.hs
@@ -1,25 +1,53 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 import GHC.Generics
-
+import Data.List.Split
+import Data.List
 import Text.Read (readMaybe)
+import System.Random
+import System.Random.Shuffle (shuffle')
 
-data Person = Person {
-      name :: String
-    , age  :: Int
+import Web.Scotty
+import Network.Wai.Middleware.RequestLogger (logStdoutDev)
+
+data Lyric = Lyric {
+      id :: Int
+    , verse :: String
+    , song  :: String
     } deriving (Generic, Show)
 
+parseLyrics :: String -> [Lyric]
+parseLyrics fileContents = do
+  [id', verse', song'] <- splitOn "|" <$> lines fileContents
+  Just id'' <- return (readMaybe id')
+  return (Lyric {Main.id = id'', verse = verse', song = song'})
+
+randomizeLyrics :: [Lyric] -> StdGen -> [Lyric]
+randomizeLyrics lyrics rng = shuffle' lyrics (length lyrics) rng
+
+--saveRandom :: [Lyric] -> [Lyric]
+
+seedGenerator :: IO()
+seedGenerator = do
+  lyrics <- parseLyrics <$> readFile "teste.txt"
+  rng <- newStdGen
+  print $ randomizeLyrics lyrics rng
+  print $ lyrics
 
 
 
-parsePeople :: String -> [Person]
-parsePeople fileContents = do
-  [name', age'] <- Prelude.words <$> Prelude.lines fileContents
-  Just age'' <- return (readMaybe age')
-  return (Person {name = name', age = age''})
 main :: IO ()
-main = do
-    people <- parsePeople <$> readFile "teste.txt"
-    putStrLn $ show(people)
-    putStrLn $ show(name( people!!0))
+main = scotty 3000 $ do
+  middleware logStdoutDev
+
+  -- Define your routes and handlers here
+  get "/hello" $ do
+    text (parseLyrics <$> readFile "teste.txt")
+--    getRandomId lyrics
+--    getRandomId lyrics
+
+--    getRandomId lyrics
+--    seedGenerator lyrics
+--    putStrLn $ show(lyrics)
+    --putStrLn $ show(verse( lyrics!!0))
     return ()
