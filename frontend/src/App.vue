9fe231ba310c8ede61541213d821abd4b55feeb6
fennerspohr <isadorafennerspohr@gmail.com>
2024-10-27 23:31:55 -0300

    novas dependências no frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,54 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref} from 'vue'
+import axios from 'axios'
+
+const getURL = "http://localhost:3000/"
+const started = ref(false)
+const isLoading = ref(false)
+const lyrics = ref()
+const currentLyric = ref()
+
+async function getLyrics(){
+  isLoading.value = true
+  started.value = true
+  axios.get(getURL + "listlyrics")
+      .then(response=>{
+        console.log(response)
+        isLoading.value = false
+        lyrics.value = response.data
+        currentLyric.value = lyrics.value[0]
+      })
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+<!--<header>-->
+  <div>
+    <div class="flex justify-center">
+      <img class="h-56" src="./assets/title.png" alt="título da página">
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="flex justify-center">
+<!--      <h4 class="text-center">Adivinhe a música da cantora Chappell Roan pela letra!</h4>-->
+    </div>
+
+    <div>
+      <div class="flex justify-center mt-4" v-if="!started">
+        <button class="bg-[#C1EEFF] hover:bg-[#99E4FF] text-[#2A1F2D] font-bold py-2 px-4 rounded" @click="getLyrics">
+          Jogar</button>
+      </div>
+
+      <div v-if="isLoading" class="mt-4">
+        <h4 class="text-center">Carregando...</h4>
+      </div>
+
+      <div v-if="lyrics">
+        {{currentLyric}}
+      </div>
+    </div>
+  </div>
+  <div></div>
+<!--</header>-->
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,22 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import tailwindcss from "tailwindcss";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+    css:{
+      postcss: {
+          plugins: [tailwindcss()],
+      },
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+    },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
